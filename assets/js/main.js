44d5d86d33b7ac899dd69078ce856f169e6cd879
Mahedi Hasan <mhasan@cleversow.com>
2020-11-14 01:07:00 +0600

    Bug fix 6

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,21 +6,21 @@ $('.site_navbar_mobile_nav_close').click(function () {
     $('.site_navbar_mobile_nav').fadeOut()
 })
 
-$('.site_navbar .dropdown').mouseenter(function () {
-    $(this).children('.dropdown_nav').slideDown()
+$('.site_navbar_mobile_nav .dropdown').click(function () {
+    $(this).children('.dropdown_nav').slideToggle()
 })
 
-$('.site_navbar .dropdown .dropdown_nav').mouseleave(function () {
-    $(this).slideUp()
+$('.site_navbar .dropdown').click(function () {
+    $(this).children('.dropdown_nav').slideToggle()
 })
 
-$('.site_navbar_mobile_nav .dropdown').mouseenter(function () {
-    $(this).children('.dropdown_nav').slideDown()
-})
+$('.site_navbar .dropdown .nav-link').append(`
+    <i class="fas fa-sort-down dropdown_icon"></i>
+`)
 
-$('.site_navbar_mobile_nav .dropdown .dropdown_nav').mouseleave(function () {
-    $(this).slideUp()
-})
+$('.site_navbar_mobile_nav .dropdown .nav-link').append(`
+    <i class="fas fa-sort-down dropdown_icon"></i>
+`)
 
 $('.btn-talk').click(function () {
     $('.contact_wrapper').css({
